ee2289ae81b9228bb0643bc788665794fecf3742
alexOlshanskyy <51353059+alexOlshanskyy@users.noreply.github.com>
2021-04-02 22:47:24 -0700

    clean up

--- src/app/SteganographyUtil.java
+++ src/app/SteganographyUtil.java
@@ -40,7 +40,6 @@ public class SteganographyUtil {
         for (int i = 4; i < finalMessage.length; i++) {
             finalMessage[i] = message[i-4];
         }
-        //System.out.println("Final Messsage: " + Arrays.toString(finalMessage));
         encryptImage((byte)bits, finalMessage);
         File outfile = new File(filename);
         try {
@@ -72,13 +71,10 @@ public class SteganographyUtil {
         }
         int i = 0;
         int remainingP = 3;
-        int remainingM = 8/bits;
         int pixel = image.getRGB(x,y);
-        //System.out.println("Pixel: " + pixel);
         int messageSize = 0;
         while (true) {
             byte val = (byte)((pixel >> ((remainingP - 1) *8))&MASKS_CLEAR[index]);
-            //System.out.println("Val: " + val);
             messageSize = (messageSize | ((int)val&MASKS_CLEAR[index])); // have to clean again 8 bit edge case
             remainingP--;
             if (i+bits == 32){
@@ -98,7 +94,6 @@ public class SteganographyUtil {
                 remainingP = 3;
             }
         }
-        //System.out.println("This is message size: " + messageSize);
         res = new byte[messageSize];
         int resI = 0;
         byte m = 0;
@@ -108,10 +103,7 @@ public class SteganographyUtil {
         }
         while (!(resI >= messageSize)) {
             byte val = (byte)((pixel >> ((remainingP - 1) *8))&MASKS_CLEAR[index]);
-            //System.out.println("VAl: " + val);
-            //System.out.println("Pixel: " + pixel);
             m = (byte)(m | val);
-            //temp = (byte)(temp & MASKS_CLEAR[index]);
             remainingP--;
             if (remainingP == 0) {
                 if (x >= image.getWidth()-1) {
@@ -124,7 +116,6 @@ public class SteganographyUtil {
                 remainingP = 3;
             }
             if (j+bits == 8){
-                //System.out.println("Here");
                 res[resI] = m;
                 resI++;
                 j = 0;
@@ -137,13 +128,11 @@ public class SteganographyUtil {
 
 
         }
-        //System.out.println("Res: " + Arrays.toString(res));
         File ff = new File(filename);
         try {
 
             OutputStream os = new FileOutputStream(ff);
             os.write(res);
-            //System.out.println("Write bytes to file.");
             os.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -153,9 +142,6 @@ public class SteganographyUtil {
     }
 
     private static boolean encryptImage(byte bits, byte[] message) {
-        //System.out.println("This is the length: " + message.length);
-        //System.out.println("This is array: " + Arrays.toString(message));
-        boolean done = false;
         int x = 1;
         int y = 0;
         int remainingP = 3;
@@ -170,45 +156,35 @@ public class SteganographyUtil {
         } else if (bits == 8) {
             index = 3;
         }
-
         byte mask = MASKS[index];
         int firstPixel = image.getRGB(0,0);
-        //System.out.println("First Before: " + firstPixel);
         firstPixel = (firstPixel & (~(MASKS_CLEAR[1])));
         firstPixel |= index;
         image.setRGB(0,0, firstPixel);
-        //System.out.println("First After: " + firstPixel);
 
         int i = 0;
         byte b = message[i];
         int pixel = image.getRGB(x, y);
-        //System.out.println("PixelBefore: " + pixel);
         int shift = 8;
         while (i < message.length) {
             int temp = (byte)((b&(mask>>(8-shift))) >> (shift-bits));
             shift -= bits;
 
             temp &= MASKS_CLEAR[index];
-            //System.out.println("Byte: " + b);
-            //System.out.println("Temp: " + temp);
             pixel &= ~(MASKS_CLEAR[index] << ((remainingP-1)*8));
             pixel |= (temp << ((remainingP-1)*8));
             remainingP--;
             remainingM--;
             if (remainingP == 0) {
                 image.setRGB(x,y,pixel);
-                //System.out.println("PixelAfter: " + pixel);
                 if (x >= image.getWidth()-1) {
                     x = 0;
                     y++;
                 } else {
                     x++;
                 }
-                //System.out.println("X: " + x);
-                //System.out.println("Y: " + y);
 
                 pixel = image.getRGB(x, y);
-                //System.out.println("PixelBefore: " + pixel);
                 remainingP = 3;
             }
             if (remainingM == 0) {
@@ -216,7 +192,6 @@ public class SteganographyUtil {
                 if (i == message.length) {
                     if (remainingP != 3){
                         image.setRGB(x,y,pixel);
-                        //System.out.println("PixelAfter: " + pixel);
                     }
                     break;
                 }
